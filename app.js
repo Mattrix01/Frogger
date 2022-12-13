8a4533dfee5458be4cbcd39c6bfd07b36c36ee2b
Mattrix01 <emjay.knight@gmail.com>
2022-12-13 14:35:39 +0000

    key adds frog to squares array

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
-const StartPauseButton = dpcument.querySelector("start-pause-button");
+const StartPauseButton = document.querySelector("start-pause-button");
 const squares = document.querySelectorAll(".grid div");
+let currentIndex = 0;
+console.log(squares);
 
-function moveFrog() {}
+function moveFrog() {
+  // adding frog into squares array when called.
+  squares[currentIndex].classList.add("frog");
+}
+// Function runs whenever a key up is heard
+document.addEventListener("keyup", moveFrog);
