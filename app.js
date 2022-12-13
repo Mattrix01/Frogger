e41ab1f01b0228f6ac66b573097f0a7abb3457d6
Mattrix01 <emjay.knight@gmail.com>
2022-12-13 14:40:32 +0000

    passing through event to see which key pressed

--- app.js
+++ app.js
@@ -2,10 +2,25 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const StartPauseButton = document.querySelector("start-pause-button");
 const squares = document.querySelectorAll(".grid div");
-let currentIndex = 0;
+let currentIndex = 76;
 console.log(squares);
 
-function moveFrog() {
+// passing through event into function (e) seeing which key
+function moveFrog(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      console.log("move left");
+      break;
+    case "ArrowRight":
+      console.log("move right");
+      break;
+    case "ArrowUp":
+      console.log("move up");
+      break;
+    case "ArrowDown":
+      console.log("move down");
+      break;
+  }
   // adding frog into squares array when called.
   squares[currentIndex].classList.add("frog");
 }
