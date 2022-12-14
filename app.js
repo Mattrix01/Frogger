4e27a6ea963ddbc09e4f95c3a4de8e59f6e500bc
Mattrix01 <emjay.knight@gmail.com>
2022-12-14 18:00:40 +0000

    logs moving with function every second interval

--- app.js
+++ app.js
@@ -43,17 +43,32 @@ function autoMoveLogs() {
   logsleft.forEach((logLeft) => moveLogLeft(logLeft));
 }
 
-autoMoveLogs();
-
 function moveLogLeft(logLeft) {
   // looking for true statements
   switch (true) {
-    //checking if log left div contains class of l1
+    //checking if log left div contains class of l1 etc.
     case logLeft.classList.contains("l1"):
       logLeft.classList.remove("l1");
       logLeft.classList.add("l2");
       break;
+    // if contains l2, remove l2 and add l3 and so on...
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
+      break;
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
   }
 }
 
-setInterval(autoMoveLogs);
+setInterval(autoMoveLogs, 1000);
