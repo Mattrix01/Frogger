5b1e7ff604bb838f950898debfccddb66d605537
Mattrix01 <emjay.knight@gmail.com>
2022-12-14 19:21:05 +0000

    satrt pause button issues wont start

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
-const StartPauseButton = document.querySelector("start-pause-button");
+const startPauseButton = document.querySelector("start-pause-button");
 const squares = document.querySelectorAll(".grid div");
 const logsLeft = document.querySelectorAll(".log-left");
 const logsRight = document.querySelectorAll(".log-right");
@@ -10,7 +10,8 @@ const carsRight = document.querySelectorAll(".car-right");
 let currentIndex = 76;
 const width = 9;
 let timerId;
-let currentTime = 10;
+let outcomeTimerId;
+let currentTime = 20;
 console.log(squares);
 
 // passing through event into function (e) seeing which key
@@ -54,16 +55,14 @@ function autoMoveElements() {
   logsRight.forEach((logRight) => moveLogRight(logRight));
   carsLeft.forEach((carLeft) => moveCarLeft(carLeft));
   carsRight.forEach((carRight) => moveCarRight(carRight));
+}
+
+// own function to check win lose condictions on faster interval than a second.
+function checkOutComes() {
   lose();
   win();
 }
-// // checking for a loser every second
-// function checkOutComes() {
-//   lose();
-//   win();
-// }
 
-//
 function moveLogLeft(logLeft) {
   // looking for true statements
   switch (true) {
@@ -180,11 +179,24 @@ function win() {
   if (squares[currentIndex].classList.contains("ending-block")) {
     resultDisplay.textContent = "You Win!";
     clearInterval(timerId);
-    // clearInterval(outcomeTimerId);
+    clearInterval(outcomeTimerId);
 
     document.removeEventListener("keyup", moveFrog);
   }
 }
 
-// running fucntion every second
-timerId = setInterval(autoMoveElements, 1000);
+// on the button click, if true there is timerId do this else this.
+startPauseButton.addEventListener("click", () => {
+  if (timerId) {
+    clearInterval(timerId);
+    clearInterval(outcomeTimerId);
+    outcomeTimerId = null;
+    timerId = null;
+    document.removeEventListener("keyup", moveFrog);
+  } else {
+    // running function every second
+    timerId = setInterval(autoMoveElements, 1000);
+    outcomeTimerId = setInterval(checkOutComes, 50);
+    document.addEventListener("keyup", moveFrog);
+  }
+});
